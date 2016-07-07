dea4251256f29b37a9739b5bf0ced955a0b2409a
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-07-07 14:30:49 -0700

    Feat:Add steps count

--- app/src/main/java/com/example/home/gym_pal/MainActivityFragment.java
+++ app/src/main/java/com/example/home/gym_pal/MainActivityFragment.java
@@ -1,15 +1,26 @@
 package com.example.home.gym_pal;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements SensorEventListener{
+
+    private SensorManager sensorManager;
+    private TextView count;
+    boolean activityRunning;
 
     public MainActivityFragment() {
     }
@@ -17,6 +28,45 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view= inflater.inflate(R.layout.fragment_main, container, false);
+
+        count = (TextView) view.findViewById(R.id.count);
+
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        activityRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this,countSensor,SensorManager.SENSOR_DELAY_UI);
+        }else {
+            Toast.makeText(getActivity(),"count sensor not available",Toast.LENGTH_LONG).show();
+        }
+        }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        activityRunning = false;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (activityRunning){
+            count.setText(String.valueOf(event.values[0]));
+
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }

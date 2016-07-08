def5b372b5f4bf9ba42d212dd93d8110337f9aba
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-07-08 11:30:53 -0700

    Feat:Add Google playservice Admobile showing Adbanner

--- app/src/main/java/com/example/home/gym_pal/MainActivity.java
+++ app/src/main/java/com/example/home/gym_pal/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+//        Adbanner
+                        AdView mAdView = (AdView) findViewById(R.id.adView);
+                // Create an ad request. Check logcat output for the hashed device ID to
+                        // get test ads on a physical device. e.g.
+                                // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+                                        AdRequest adRequest = new AdRequest.Builder()
+                                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                                .build();
+               mAdView.loadAd(adRequest);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {

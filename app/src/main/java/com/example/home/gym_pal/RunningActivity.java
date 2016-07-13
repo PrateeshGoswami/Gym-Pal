9fe3aa1e395096f4a02b4ffe9dba7179b96dcd7c
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-07-13 00:52:04 -0700

    add google location services

--- app/src/main/java/com/example/home/gym_pal/RunningActivity.java
+++ app/src/main/java/com/example/home/gym_pal/RunningActivity.java
@@ -1,17 +1,28 @@
 package com.example.home.gym_pal;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,7 +34,9 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class RunningActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class RunningActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    protected static final String TAG = "RunningActivity";
 
     GPSTracker gps;
     private GoogleMap mMap;
@@ -35,6 +48,7 @@ public class RunningActivity extends AppCompatActivity implements OnMapReadyCall
     @Bind(R.id.button_find_location)
     public Button mbuttonFindLoc;
 
+
     @Bind(R.id.toolbar)
     public Toolbar mToolbar;
 
@@ -43,7 +57,22 @@ public class RunningActivity extends AppCompatActivity implements OnMapReadyCall
 
     private LocationManager locationManager;
     private LocationListener locationListener;
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
 
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
+
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
+    private double lat;
+    private double lon;
 
 
     @Override
@@ -68,11 +97,11 @@ public class RunningActivity extends AppCompatActivity implements OnMapReadyCall
             public void onClick(View v) {
                 gps = new GPSTracker(RunningActivity.this);
 
-                if (gps.canGetLocation()){
+                if (gps.canGetLocation()) {
                     double latitude = gps.getLatitude();
                     double longitude = gps.getLongitude();
 
-                    mText_location.append("\n" + latitude +"\n" + longitude);
+                    mText_location.append("\n" + latitude + "\n" + longitude);
 
                 }
             }
@@ -81,8 +110,33 @@ public class RunningActivity extends AppCompatActivity implements OnMapReadyCall
                 .findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(this);
 
+        buildGoogleApiClient();
 
+    }
 
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     @OnClick(R.id.fab)
@@ -93,16 +147,79 @@ public class RunningActivity extends AppCompatActivity implements OnMapReadyCall
 
 
     @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            lat = mLastLocation.getLatitude();
+            lon = mLastLocation.getLongitude();
+//            mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
+//                    mLastLocation.getLatitude()));
+//            mLongitudeText.setText(String.format("%s: %f", mLongitudeLabel,
+//                    mLastLocation.getLongitude()));
+        } else {
+            Toast.makeText(this, "no_location_detected ", Toast.LENGTH_LONG).show();
+        }
+        Log.d("test1", "latitude" + lat + "longi" + lon);
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        GPSTracker gpsTracker = new GPSTracker(getApplicationContext());
-        float lat =(float)Math.round(gpsTracker.getLatitude());
-        float lon = (float)Math.round(gpsTracker.getLongitude());
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            lat = mLastLocation.getLatitude();
+            lon = mLastLocation.getLongitude();
+//            mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
+//                    mLastLocation.getLatitude()));
+//            mLongitudeText.setText(String.format("%s: %f", mLongitudeLabel,
+//                    mLastLocation.getLongitude()));
+        } else {
+            Toast.makeText(this, "no_location_detected ", Toast.LENGTH_LONG).show();
+        }
+        Log.d("test2", "latitude" + lat + "longi" + lon);
+//        GPSTracker gpsTracker = new GPSTracker(getApplicationContext());
+//        float lat = (float) Math.round(gpsTracker.getLatitude());
+//        float lon = (float) Math.round(gpsTracker.getLongitude());
 
         // Add a marker in Sydney, Australia, and move the camera.
         LatLng sydney = new LatLng(lat, lon);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
     }
 }

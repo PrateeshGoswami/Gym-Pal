da8e0fad0882ee9751577f601d12ce592a543df2
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-07-14 10:19:33 -0700

    Tested Marker moves with location change

--- app/src/main/java/com/example/home/gym_pal/RunningActivity.java
+++ app/src/main/java/com/example/home/gym_pal/RunningActivity.java
@@ -180,6 +180,10 @@ public class RunningActivity extends AppCompatActivity implements LocationListen
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
         mMap.animateCamera(cameraUpdate);
+        if (currLocationMarker!=null){
+            currLocationMarker.remove();
+        }
+       
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));

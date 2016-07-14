24bb543d1cec17efd39c44df3a8e25d5ca9ca8e5
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-07-13 19:01:33 -0700

    Feat:Running activity recieves current user location

--- app/src/main/java/com/example/home/gym_pal/RunningActivity.java
+++ app/src/main/java/com/example/home/gym_pal/RunningActivity.java
@@ -1,225 +1,163 @@
 package com.example.home.gym_pal;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+public class RunningActivity extends AppCompatActivity implements LocationListener
+{
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
 
-public class RunningActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private LocationRequest mLocationRequest;
+    private TextView latitude, longitude;
 
-    protected static final String TAG = "RunningActivity";
+    private double fusedLatitude = 0.0;
+    private  double fusedLongitude = 0.0;
 
-    GPSTracker gps;
-    private GoogleMap mMap;
-
-
-    @Bind(R.id.text_location)
-    public TextView mText_location;
-
-    @Bind(R.id.button_find_location)
-    public Button mbuttonFindLoc;
-
-
-    @Bind(R.id.toolbar)
-    public Toolbar mToolbar;
-
-    @Bind(R.id.adView)
-    public AdView mAdview;
-
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-    /**
-     * Provides the entry point to Google Play services.
-     */
-    protected GoogleApiClient mGoogleApiClient;
-
-    /**
-     * Represents a geographical location.
-     */
-    protected Location mLastLocation;
-
-    protected String mLatitudeLabel;
-    protected String mLongitudeLabel;
-    protected TextView mLatitudeText;
-    protected TextView mLongitudeText;
-    private double lat;
-    private double lon;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running);
-        ButterKnife.bind(this);
+        initializeViews();
 
-        setSupportActionBar(mToolbar);
-        //        Adbanner
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdview.loadAd(adRequest);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        mbuttonFindLoc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gps = new GPSTracker(RunningActivity.this);
-
-                if (gps.canGetLocation()) {
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-
-                    mText_location.append("\n" + latitude + "\n" + longitude);
-
-                }
-            }
-        });
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_fragment);
-        mapFragment.getMapAsync(this);
-
-        buildGoogleApiClient();
+        if (checkPlayServices()) {
+            startFusedLocation();
+            registerRequestUpdate(this);
+        }
 
     }
 
-    /**
-     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
-     */
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
+    private void initializeViews() {
+        latitude = (TextView) findViewById(R.id.textview_latitude);
+        longitude = (TextView) findViewById(R.id.textview_longitude);
     }
 
     @Override
     protected void onStop() {
+        stopFusedLocation();
         super.onStop();
-        if (mGoogleApiClient.isConnected()) {
+    }
+
+
+    // check if google play services is installed on the device
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                Toast.makeText(getApplicationContext(),
+                        "This device is supported. Please download google play services", Toast.LENGTH_LONG)
+                        .show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+    public void startFusedLocation() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API)
+                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                        @Override
+                        public void onConnectionSuspended(int cause) {
+                        }
+
+                        @Override
+                        public void onConnected(Bundle connectionHint) {
+
+                        }
+                    }).addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+
+                        @Override
+                        public void onConnectionFailed(ConnectionResult result) {
+
+                        }
+                    }).build();
+            mGoogleApiClient.connect();
+        } else {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    public void stopFusedLocation() {
+        if (mGoogleApiClient != null) {
             mGoogleApiClient.disconnect();
         }
     }
 
-    @OnClick(R.id.fab)
-    public void setfabclick() {
-        Snackbar.make(getCurrentFocus(), "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+
+    public void registerRequestUpdate(final LocationListener listener) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000); // every second
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, listener);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    if (!isGoogleApiClientConnected()) {
+                        mGoogleApiClient.connect();
+                    }
+                    registerRequestUpdate(listener);
+                }
+            }
+        }, 1000);
     }
 
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            lat = mLastLocation.getLatitude();
-            lon = mLastLocation.getLongitude();
-//            mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
-//                    mLastLocation.getLatitude()));
-//            mLongitudeText.setText(String.format("%s: %f", mLongitudeLabel,
-//                    mLastLocation.getLongitude()));
-        } else {
-            Toast.makeText(this, "no_location_detected ", Toast.LENGTH_LONG).show();
-        }
-        Log.d("test1", "latitude" + lat + "longi" + lon);
+    public boolean isGoogleApiClientConnected() {
+        return mGoogleApiClient != null && mGoogleApiClient.isConnected();
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            lat = mLastLocation.getLatitude();
-            lon = mLastLocation.getLongitude();
-//            mLatitudeText.setText(String.format("%s: %f", mLatitudeLabel,
-//                    mLastLocation.getLatitude()));
-//            mLongitudeText.setText(String.format("%s: %f", mLongitudeLabel,
-//                    mLastLocation.getLongitude()));
-        } else {
-            Toast.makeText(this, "no_location_detected ", Toast.LENGTH_LONG).show();
-        }
-        Log.d("test2", "latitude" + lat + "longi" + lon);
-//        GPSTracker gpsTracker = new GPSTracker(getApplicationContext());
-//        float lat = (float) Math.round(gpsTracker.getLatitude());
-//        float lon = (float) Math.round(gpsTracker.getLongitude());
+    public void onLocationChanged(Location location) {
+        setFusedLatitude(location.getLatitude());
+        setFusedLongitude(location.getLongitude());
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(lat, lon);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        Toast.makeText(getApplicationContext(), "NEW LOCATION RECEIVED", Toast.LENGTH_LONG).show();
 
-    }
-    @Override
-    public void onConnectionSuspended(int i) {
-        // The connection to Google Play services was lost for some reason. We call connect() to
-        // attempt to re-establish the connection.
-        Log.i(TAG, "Connection suspended");
-        mGoogleApiClient.connect();
-
+        latitude.setText(getString(R.string.latitude_string) +" "+ getFusedLatitude());
+        longitude.setText(getString(R.string.longitude_string) +" "+ getFusedLongitude());
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-        // onConnectionFailed.
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+    public void setFusedLatitude(double lat) {
+        fusedLatitude = lat;
+    }
+
+    public void setFusedLongitude(double lon) {
+        fusedLongitude = lon;
+    }
+
+    public double getFusedLatitude() {
+        return fusedLatitude;
+    }
+
+    public double getFusedLongitude() {
+        return fusedLongitude;
     }
 }
+
